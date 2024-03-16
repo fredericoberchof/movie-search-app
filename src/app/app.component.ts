15514178c599595a076c35785e7d011f6ac26ae2
Frederico Berchof <fredericoberchof@Fredericos-MacBook-Air.local>
2024-03-16 14:53:38 -0300

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MovieSearchComponent } from "./movie-search/movie-search.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, HeaderComponent, MovieSearchComponent]
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MovieSearchComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'movie-search-app';

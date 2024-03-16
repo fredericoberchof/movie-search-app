e75755bc21abc079596e389e93165a6127e41cf3
Frederico Berchof <fredericoberchof@Fredericos-MacBook-Air.local>
2024-03-16 15:07:22 -0300

    merge footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,21 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
-<<<<<<< HEAD
-=======
 import { MovieSearchComponent } from "./movie-search/movie-search.component";
->>>>>>> origin/movie-search
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-<<<<<<< HEAD
-    imports: [CommonModule, RouterOutlet, HeaderComponent]
-=======
-    imports: [CommonModule, RouterOutlet, HeaderComponent, MovieSearchComponent]
->>>>>>> origin/movie-search
+    imports: [CommonModule, RouterOutlet, HeaderComponent, MovieSearchComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'movie-search-app';
